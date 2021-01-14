969eeaad2af0c2b721d55955631dacf6cc9ad384
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-01-14 13:38:40 +0100

    Add project files.

---
+++ Moneymoneymoney/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Moneymoneymoney
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Dit is mijn template - Een console app in C#
+        }
+    }
+}
